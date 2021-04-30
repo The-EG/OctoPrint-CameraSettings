222b8762af268d07a7d420ebed990e85a65c76f8
Taylor Talkington <taylor.talkington@gmail.com>
2021-04-30 08:13:36 -0400

    Add LED Mode and LED Frequency controls
    Requested for multiple cameras:
     - Logitech C270 HD (fix #16)
     - Logitech StreamCam (fix #17)
     - Logitech C922 Pro Stream (fix #22)

--- octoprint_CameraSettings/static/js/CameraSettings.js
+++ octoprint_CameraSettings/static/js/CameraSettings.js
@@ -66,6 +66,8 @@ $(function() {
             gamma: { use: ko.observable(false), value: ko.observable(undefined), min: ko.observable(0), max: ko.observable(100), step: ko.observable(1) },
             focus_auto: { use: ko.observable(false), value: ko.observable(undefined) },
             privacy: { use: ko.observable(false), value: ko.observable(undefined) },
+            led1_mode: { use: ko.observable(false), value: ko.observable(undefined), values: ko.observableArray([]) },
+            led1_frequency: { use: ko.observable(false), value: ko.observable(undefined), min: ko.observable(0), max: ko.observable(100), step: ko.observable(1) },
         };
 
         self.shouldUpdateSettings = false;
